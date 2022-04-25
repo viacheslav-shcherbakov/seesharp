7f9b7d4f96b3c76daa440be59d7b4775da0d796e
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-04-25 17:33:20 +0300

    Added two new examples

---
+++ example006_dots/Program.cs
@@ -0,0 +1,12 @@
+﻿//The program about dots
+Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xA = 1, yA = 1, xB = 1, yB = 20, xC = 30, yC = 20;
+Console.SetCursorPosition(xA, yA);
+Console.WriteLine("+");
+Console.SetCursorPosition(xB, yB);
+Console.WriteLine("+");
+Console.SetCursorPosition(xC, yC);
+Console.WriteLine("+");
---
+++ example007/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите диапазон чисел");
+int n2 = int.Parse(Console.ReadLine());
+int n1 = -n2;
+Console.WriteLine(n1);
+while (n1<= n2)
+{
+
+    Console.Write(n1);
+    Console.Write(" ");
+    n1 ++;
+}
