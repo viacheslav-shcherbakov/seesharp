1cbe261c147916ba5e488033f68102e14efbfcab
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-04-26 00:43:16 +0300

    Homework 9: the third number

---
+++ homework9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Clear();
+Console.Write("Enter the number -> ");
+int number = int.Parse(Console.ReadLine() ?? "");
+if (number / 100 > 1)
+{
+    Console.WriteLine($"The result is {number %10}");
+}
+else{
+    Console.WriteLine("There is no third number at all!");
+}
