5121e50a3ce98ffa8ae2620629d6a95e5e67b545
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-04-25 17:44:59 +0300

    Example 1. Learning, how to write to the console.

--- example001_helloconcole/Program.cs
+++ example001_helloconcole/Program.cs
@@ -1,2 +1,6 @@
-﻿
-Console.WriteLine("Hello, ");
+﻿//This is a simple Hello world program
+Console.WriteLine("Hello, world (first time)! ->  " + "Writes a line and goes to the next line"); //Writes a line and goes to the next line
+Console.Write("Hello, world (second time)! ->  " + "Cursor remains at the same line ->  "); //Cursor remains at the same line
+Console.Write("Hello, world (third time)! Really?!");
+
+//No functions - no algorythms
