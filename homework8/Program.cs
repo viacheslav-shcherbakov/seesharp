99ce64b4dee63245ad1b95ecb6d6c46756625c72
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-04-26 00:36:54 +0300

    Homework 8: the remainder

---
+++ homework8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Clear();
+int number = 3;
+Console.Write("Enter a number -> ");
+int userNumber = int.Parse(Console.ReadLine() ?? "");
+
+if (userNumber % number == 0)
+{
+    Console.WriteLine($"Number {userNumber} is devisible by {number}");
+}
+else 
+{
+    Console.Write($"Number {userNumber} is not devisible by {number}. The remainder is " + userNumber % number);
+}
