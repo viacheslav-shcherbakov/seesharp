44bc49323fa14a2859bfdfda4164c6798f00e0db
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-04-25 17:59:59 +0300

    Example 4. Learning, how to divide two numbers (not integers)

--- example004_chastnoye/Program.cs
+++ example004_chastnoye/Program.cs
@@ -1,4 +1,5 @@
 ﻿// New example on math
-double numA = 12;
-double numB = 5;
-Console.WriteLine(numA / numB);
+Console.Write("Enter two numbers to divide ->>  ");
+double numA = double.Parse(Console.ReadLine() ?? "");
+double numB = double.Parse(Console.ReadLine() ?? "");
+Console.WriteLine($"We've divided {numA} / {numB} , the result is " + numA / numB);
