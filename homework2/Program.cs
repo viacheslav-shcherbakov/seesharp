bec6cc2df2afec4a043bc98e8b6140c4e0fd3852
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-04-25 21:28:49 +0300

    Added homewor 2: Days of the week

---
+++ homework2/Program.cs
@@ -0,0 +1,15 @@
+﻿//По заданному номеру дня недели вывести его название
+Console.Clear();
+string[] dayofweekArray = new string [7]{ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satturday", "Sunday"};
+Console.WriteLine("Wellcome to the game: Which day of the week!");
+Console.Write("Enter your name ->>  ");
+string name = Console.ReadLine() ?? "";
+Console.Write($"OK, {name}, enter a number from 1 to 7: -> ");
+int number = int.Parse(Console.ReadLine() ?? "");
+if (number < 1 && number > 7)
+{
+    Console.WriteLine($"{number} is a wrong number!");
+}
+else{
+    Console.WriteLine("It's " + dayofweekArray[number - 1] + " !");
+}
