62440f7c1a27f07c400f26704a55876a220b0dd4
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-04-25 17:34:32 +0300

    Added examles 4 & 8

--- example004_chastnoye/Program.cs
+++ example004_chastnoye/Program.cs
@@ -1,2 +1,4 @@
 ﻿// New example on math
-Console.WriteLine("Hello, World!");
+double numA = 12;
+double numB = 5;
+Console.WriteLine(numA / numB);
---
+++ example008_last/Program.cs
@@ -0,0 +1,9 @@
+﻿//Console.Write("Введите трехзначное число -> ");
+//int n = int.Parse(Console.ReadLine());
+//Console.Write("Последняя цифра чила -> ");
+//Console.WriteLine(n % 10);
+Console.Write("What is your name? -> ");
+string name = Console.ReadLine() ?? "";
+Console.Write("How old are you? -> ");
+int age = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Your name is {name}. Your age is {age} years old.");
