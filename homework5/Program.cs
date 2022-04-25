a8947cfcca3110233329989be6b1b7033e76304b
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-04-25 22:13:29 +0300

    Homework 5: the second number of three

---
+++ homework5/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число -> ");
+int n = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Вторая цифра чила -> ");
+Console.WriteLine((n /10) % 10);
