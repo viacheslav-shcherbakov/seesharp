75005e13bd451ff766be913add667e0ac268641b
Viacheslav Shcherbakov <65897497+viacheslav-shcherbakov@users.noreply.github.com>
2022-04-27 10:26:47 +0300

    Added >100 <1000 check

--- homework9/Program.cs
+++ homework9/Program.cs
@@ -2,10 +2,10 @@
 Console.Clear();
 Console.Write("Enter the number -> ");
 int number = int.Parse(Console.ReadLine() ?? "");
-if (number / 100 > 1)
+if (number / 100 > 1 && number <1000 && number >= 100)
 {
     Console.WriteLine($"The result is {number %10}");
 }
 else{
-    Console.WriteLine("There is no third number at all!");
+    Console.WriteLine("There is no third number at all! \n May be because your number is less then 100 or bigger than 999?");
 }
