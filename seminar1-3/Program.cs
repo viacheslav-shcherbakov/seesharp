b30ab619067a62764bfcbb8c2c7affed6f65abbe
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-05-12 12:06:14 +0300

    Seminar palindrom

---
+++ seminar1-3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Enter a number");
+int x = int.Parse(Console.ReadLine() ?? "");
+if (x/10000 == x%10 && x / 1000 % 10 == x /10 % 10)
+    Console.WriteLine("Palindrom");
+else
+    Console.WriteLine("not palindrom");
