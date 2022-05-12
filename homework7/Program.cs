51d0295a65c01e0eab0496153e5123b5807c405d
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-05-12 12:44:30 +0300

    added foolproof to HW7

--- homework7/Program.cs
+++ homework7/Program.cs
@@ -5,6 +5,13 @@ Console.Write("Enter your name ->>  ");
 string name = Console.ReadLine() ?? "";
 Console.Write($"OK, {name}, enter the number from 100 to 999 ->>  ");
 int number = int.Parse(Console.ReadLine() ?? "");
-int num1 = number / 100;
-int num2 = number % 10;
-Console.WriteLine($"It was {number} and now it's {num1}{num2}. How cool is this?");
+
+if (number/1000 > 10 || number/ 100 == 0)
+{
+    Console.Write($"{number} - not a 3-digit number!");
+}
+else{
+    int num1 = number / 100;
+    int num2 = number % 10;
+    Console.WriteLine($"It was {number} and now it's {num1}{num2}. How cool is this?");
+}
