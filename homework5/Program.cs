de56248abb9f677058043955f0c6b5bcebf82da4
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-05-12 12:39:48 +0300

    fixed HW5

--- homework5/Program.cs
+++ homework5/Program.cs
@@ -2,5 +2,12 @@
 Console.Clear();
 Console.Write("Введите трехзначное число -> ");
 int n = int.Parse(Console.ReadLine() ?? "");
-Console.Write("Вторая цифра чила -> ");
-Console.WriteLine((n /10) % 10);
+if (n/1000 > 10 || n/ 100 == 0)
+{
+    Console.Write($"{n} - это не трехзначное число!");
+
+}
+else{
+    Console.Write("Вторая цифра чила -> ");
+    Console.WriteLine((n /10) % 10);
+}
