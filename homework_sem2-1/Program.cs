82454f174522aa9f62bdbc2ea25c9f9bf0570058
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-05-17 16:28:08 +0300

    Added HW 1 & 2  for sem 2

---
+++ homework_sem2-1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Clear();
+Console.Write("Введите число от 1 до 7 ->> ");
+int day_week_num = int.Parse(Console.ReadLine());
+if (day_week_num >=6)
+{
+    Console.WriteLine($"{day_week_num}-ый день недели - это выходной");
+}
+else
+{
+    Console.WriteLine($"{day_week_num}-ый день недели - это не выходной");
+}
---
+++ homework_sem2-2/Program.cs
@@ -0,0 +1,18 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.Clear();
+Console.Write("Введите первое число ->> ");
+int first_num = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ->> ");
+int second_num = int.Parse(Console.ReadLine());
+if (first_num == second_num*second_num)
+{
+    Console.WriteLine($"{first_num} - это {second_num} возведенное в квадрат");
+}
+else if (first_num*first_num == second_num)
+{
+    Console.WriteLine($"{second_num} - это {first_num} возведенное в квадрат");
+}
+else
+{
+    Console.WriteLine("Эти числа не связаны 'квадратными' отношениями ))");
+}
