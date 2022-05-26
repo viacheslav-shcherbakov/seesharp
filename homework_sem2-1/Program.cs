a8adf001d756b7653f87aca9b1e905559e0e4bad
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-05-26 17:46:19 +0300

    Added exeptions

--- homework_sem2-1/Program.cs
+++ homework_sem2-1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
 Console.Clear();
 Console.Write("Введите число от 1 до 7 ->> ");
-int day_week_num = int.Parse(Console.ReadLine());
+int day_week_num = int.Parse(Console.ReadLine() ?? "0");
 if (day_week_num >=6)
 {
     Console.WriteLine($"{day_week_num}-ый день недели - это выходной");
--- homework_sem2-2/Program.cs
+++ homework_sem2-2/Program.cs
@@ -1,9 +1,9 @@
 ﻿// По двум заданным числам проверять является ли одно квадратом другого
 Console.Clear();
 Console.Write("Введите первое число ->> ");
-int first_num = int.Parse(Console.ReadLine());
+int first_num = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите второе число ->> ");
-int second_num = int.Parse(Console.ReadLine());
+int second_num = int.Parse(Console.ReadLine() ?? "0");
 if (first_num == second_num*second_num)
 {
     Console.WriteLine($"{first_num} - это {second_num} возведенное в квадрат");
