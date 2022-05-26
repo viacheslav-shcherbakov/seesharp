14bef2f47b4b9f6b0a3eb22794e8e127bdd305c5
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-05-26 17:44:23 +0300

    Added seminar 6

--- seminar6-1/Program.cs
+++ seminar6-1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Матрица
+//В двумерном массиве n×k заменить четные элементы на противоположные
 
+Console.Clear();
 void PrintArray(int[,] matr)
 {
     for (int i=0; i<matr.GetLength(0); i++)
@@ -17,15 +19,42 @@ void FillArray(int[,] matr)
     {
         for(int j=0; j<matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().Next(10,99);
+            matr[i,j] = new Random().Next(-99,99);          
         }
     }
 }
+void ChangeArray(int[,] matr)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for(int j=0; j<matr.GetLength(1); j++)
+        {
+            if(matr[i,j]%2==0)
+            {
+                matr[i,j] = - matr[i,j];
+            }            
+        }
+    }
+}
+//В двумерном массиве показать позицию числа, заданного пользователем или указать, что такого элемента нет
+bool IsInArray(int[,] matr, int number)
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+        for(int j=0; j<matr.GetLength(1); j++)
+            if(matr[i,j]==number) return true; 
+    return false;
+}
+
 Console.WriteLine("Введите число m: ");
 int m = int.Parse(Console.ReadLine()?? "");
 Console.WriteLine("Введите число n: ");
 int n = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine("Какое число искать? ");
+int number = int.Parse(Console.ReadLine()?? "");
 
 int[,] matrix = new int[m,n];
 FillArray(matrix);
+Console.WriteLine("Начальный массив: ");
 PrintArray(matrix);
+if(IsInArray(matrix,number)==true) Console.WriteLine("Нашел!");
+else Console.WriteLine("Не нашел!");
