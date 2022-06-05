07960e3e41f4b5419e0d906ca3466b78f1ae60e7
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-06-05 23:39:54 +0300

    Fixed sem6

--- seminar6-1/Program.cs
+++ seminar6-1/Program.cs
@@ -4,57 +4,58 @@
 Console.Clear();
 void PrintArray(int[,] matr)
 {
-    for (int i=0; i<matr.GetLength(0); i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for(int j=0; j<matr.GetLength(1); j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            Console.Write($"{matr[i,j]} ");
+            Console.Write($"{matr[i, j]} ");
         }
-    Console.WriteLine();
+        Console.WriteLine();
     }
 }
 void FillArray(int[,] matr)
 {
-    for (int i=0; i<matr.GetLength(0); i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for(int j=0; j<matr.GetLength(1); j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().Next(-99,99);          
+            matr[i, j] = new Random().Next(-99, 99);
         }
     }
 }
-void ChangeArray(int[,] matr)
+/* void ChangeArray(int[,] matr)
 {
-    for (int i=0; i<matr.GetLength(0); i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for(int j=0; j<matr.GetLength(1); j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            if(matr[i,j]%2==0)
+            if (matr[i, j] % 2 == 0)
             {
-                matr[i,j] = - matr[i,j];
-            }            
+                matr[i, j] = -matr[i, j];
+            }
         }
     }
-}
+} */
+
 //В двумерном массиве показать позицию числа, заданного пользователем или указать, что такого элемента нет
 bool IsInArray(int[,] matr, int number)
 {
-    for (int i=0; i<matr.GetLength(0); i++)
-        for(int j=0; j<matr.GetLength(1); j++)
-            if(matr[i,j]==number) return true; 
+    for (int i = 0; i < matr.GetLength(0); i++)
+        for (int j = 0; j < matr.GetLength(1); j++)
+            if (matr[i, j] == number) return true;
     return false;
 }
 
 Console.WriteLine("Введите число m: ");
-int m = int.Parse(Console.ReadLine()?? "");
+int m = int.Parse(Console.ReadLine() ?? "");
 Console.WriteLine("Введите число n: ");
-int n = int.Parse(Console.ReadLine()?? "");
+int n = int.Parse(Console.ReadLine() ?? "");
 Console.WriteLine("Какое число искать? ");
-int number = int.Parse(Console.ReadLine()?? "");
+int number = int.Parse(Console.ReadLine() ?? "");
 
-int[,] matrix = new int[m,n];
+int[,] matrix = new int[m, n];
 FillArray(matrix);
 Console.WriteLine("Начальный массив: ");
 PrintArray(matrix);
-if(IsInArray(matrix,number)==true) Console.WriteLine("Нашел!");
+if (IsInArray(matrix, number) == true) Console.WriteLine("Нашел!");
 else Console.WriteLine("Не нашел!");
