c25d233709ddfcd3c8ed49fe9bb1377b2653d7f6
Viacheslav Shcherbakov <fargofar@gmail.com>
2022-06-30 11:17:18 +0300

    At last

--- choose_your_character/Program.cs
+++ choose_your_character/Program.cs
@@ -1,2 +1,29 @@
 ﻿// блок-схема
-// 
+// input_array[]
+// output_array []
+// input str
+// add str to array
+// for items in input_array chose item where item.length <= 3 & add to output_array
+// print output_array
+
+Console.Clear();
+Console.Write("Enter the number of elements to test: ");
+int elements_number = int.Parse(Console.ReadLine() ?? "");
+string [] input_array = new string [elements_number];
+string [] output_array = new string [elements_number];
+
+for(int i=0; i<input_array.Length; i++)
+{
+    Console.Write($"Enter a line #{i+1}: ");
+    string input = Console.ReadLine() ?? "";
+    input_array[i] = input;  
+}
+Console.Write("Here are the results (strings less then or equal to 3 characters long) ");
+for(int i=0; i<input_array.Length; i++)
+{
+    if(input_array[i].Length <= 3)
+    {        
+        output_array[i] = input_array[i];
+        Console.Write(output_array[i] + ", ");
+    }
+}
---
+++ seminar9/Program.cs
@@ -0,0 +1,31 @@
+﻿// Семинар 9 Найти сумму цифр числа
+
+Console.Clear();
+int SumOfNumbers(int n) => n==0 ? n : n%10 + SumOfNumbers(n/10);       
+//Console.WriteLine(SumOfNumbers(n));
+
+
+
+
+//Написать программу возведения числа А в целую стень B
+
+double Amount(int a, int b) => b==1 ? a : a*Amount(a,b-1);
+
+Console.Write("Введите число, которое надо возвести в степень: ");
+int a = int.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine(Amount(a,b));
+
+// Написать программу показывающие первые N чисел, 
+// для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+int Fibbonachi(int c, int n)
+{
+    if(n==0) return c;    
+    return c + Fibbonachi(c,n-1);
+}
+int l = 10;
+int c = a+b;
+Console.Write(Fibbonachi(c,l));
